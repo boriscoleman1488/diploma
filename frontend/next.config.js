14c9cccf3a2236b28303f39a55c4b779c3834d07
boriscoleman1488 <boriscoleman1488@gmail.com>
2025-06-23 15:31:23 +0300

    Updated package.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ['localhost', 'api.pexels.com', 'images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 }
 
 module.exports = nextConfig
