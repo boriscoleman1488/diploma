52784754644279d279f568ce00d3900fe4d9f652
boriscoleman1488 <boriscoleman1488@gmail.com>
2025-06-23 15:26:47 +0300

    Fix Next.js Configuration Issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost', 'api.pexels.com', 'images.pexels.com'],
     remotePatterns: [
